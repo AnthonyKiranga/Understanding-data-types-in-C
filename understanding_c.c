0ce1f712347ffe86fbd5fee2e2e99eeacd8570af
Anthony <kirangaantony@gmail.com>
2025-02-05 14:33:20 +0300

    A c program to help undersnad Variables

--- understanding_c.c
+++ understanding_c.c
@@ -2,7 +2,17 @@
 
 int main(){
 
-    printf("I like pizza!\n");
-    printf("It's really good!\n");
+int age = 29;
+float gpa = 90.5;
+char grade = 'A';
+char name[] = "Anthony";
+
+printf("You are %d years old\n", age);
+printf("Your average score is %f\n", gpa);
+printf("You've scored an %c\n", grade);
+printf("Your name is %s\n", name);
+
     return 0;
 }
+
+
